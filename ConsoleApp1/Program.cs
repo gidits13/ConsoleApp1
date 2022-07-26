028c346f3147b65a4e14b3d366deea1e50cd15ca
gidits13 <gidits13@gmail.com>
2022-07-26 19:04:34 +0300

    Типы данных

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,11 +5,15 @@ class MainClass
 	public static void Main(string[] args)
 	{
 
-		string myName;
-		myName = "Jane";
+		string MyName = "Jane";
+		byte MyAge = 27;
+		bool HaveIApet = true;
+		double MyShoeSize = 37.5;
 
-		Console.WriteLine(myName);
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + HaveIApet);
+		Console.WriteLine("My shoe size is " + MyShoeSize);
 
-		Console.ReadKey();
 	}
 }
